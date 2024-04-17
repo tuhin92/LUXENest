8dcfa791ff6fe01cee10364edd4e4c1bfa7e907f
tuhin92 <tuhinalmamun71m@gmail.com>
2024-04-17 20:28:13 +0600

    added customer review

--- src/CutomerReview.jsx
+++ src/CutomerReview.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const CutomerReview = () => {
   return (
-    <div className="container mx-auto my-12">
+    <div className="container mx-auto my-12 grid md:grid-cols-2 lg:grid-cols-3 gap-6 ">
       {/* review 1  */}
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
           <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            src="https://i.ibb.co/2qCTB9K/4522921ee9c0-jack-nicholson-smiling-jpg.webp"
             alt="Shoes"
           />
         </figure>
@@ -15,7 +15,41 @@ const CutomerReview = () => {
           <h2 className="card-title">Jack</h2>
           <p className="text-gray-600">That was amazing, i'm totally satisfied with their service</p>
           <div className="card-actions justify-end">
-            <p>Rating: 4.5 / 5.00</p>
+            <p className="text-gray-700 font-bold">Rating: 4.5 / 5.00</p>
+          </div>
+        </div>
+      </div>
+
+      {/* review 2  */}
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://i.ibb.co/QQvwmGH/killer-mike-with-mic.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Mike</h2>
+          <p className="text-gray-600">That was amazing, i'm totally satisfied with their service</p>
+          <div className="card-actions justify-end">
+            <p className="text-gray-700 font-bold">Rating: 4.5 / 5.00</p>
+          </div>
+        </div>
+      </div>
+
+      {/* review 3  */}
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://i.ibb.co/bR3MrNT/Web-Bio22-Marvin-Gomez.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Cupper</h2>
+          <p className="text-gray-600">That was amazing, i'm totally satisfied with their service</p>
+          <div className="card-actions justify-end">
+            <p className="text-gray-700 font-bold">Rating: 4.5 / 5.00</p>
           </div>
         </div>
       </div>
